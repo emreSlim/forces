00ff629724115b506a09b5f7cc07c099bba6b98a
emreSlim <emraanqureshi@gmail.com>
2022-10-31 00:04:09 +0530

    seperate canvas from app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import React from "react";
-import { Circle } from "./shapes";
+import { Canvas } from "./components";
 
-class App extends React.Component {
-  private readonly CANVAS_ID = "canvas";
-  private readonly WIDTH = window.innerWidth;
-  private readonly HEIGHT = window.innerHeight - 100;
-  private ctx: CanvasRenderingContext2D | null = null;
-
-  componentDidMount = () => {
-    console.log("mount");
-    const canvas = document.getElementById(
-      this.CANVAS_ID
-    ) as HTMLCanvasElement | null;
-
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      this.ctx = ctx;
-      if (ctx) {
-        this.drawCircle();
-      }
-    }
-  };
-
-  drawCircle = () => {
-    if (this.ctx) {
-      const circle = new Circle(50);
-      circle.setFillColor("red");
-      circle.setPosition({ x: 100, y: 100 });
-      circle.draw(this.ctx);
-    }
-  };
-  render = () => {
-    return (
-      <div className="App">
-        <h1>Forces</h1>
-        <canvas width={this.WIDTH} height={this.HEIGHT} id={this.CANVAS_ID} />
-      </div>
-    );
-  };
-}
-
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Forces</h1>
+      <Canvas />
+    </div>
+  );
+};
 export default App;
---
+++ src/components/canvas/canvas.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Circle } from "../../shapes";
+
+class Canvas extends React.Component {
+  private readonly CANVAS_ID = "canvas";
+  private readonly WIDTH = window.innerWidth;
+  private readonly HEIGHT = window.innerHeight - 100;
+  private ctx: CanvasRenderingContext2D | null = null;
+
+  componentDidMount = () => {
+    console.log("mounted");
+    const canvas = document.getElementById(
+      this.CANVAS_ID
+    ) as HTMLCanvasElement | null;
+
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      this.ctx = ctx;
+      if (ctx) {
+        this.drawCircle();
+      }
+    }
+  };
+
+  drawCircle = () => {
+    if (this.ctx) {
+      const circle = new Circle(50);
+      circle.setFillColor("red");
+      circle.setPosition({ x: 100, y: 100 });
+      circle.draw(this.ctx);
+    }
+  };
+  render = () => {
+    return (
+      <canvas width={this.WIDTH} height={this.HEIGHT} id={this.CANVAS_ID} />
+    );
+  };
+}
+
+export { Canvas };
